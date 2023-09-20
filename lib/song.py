3065ff05b787509448946e01bfe56521e86c064a
Ochuodho <ochuodhoderrick@gmail.com>
2023-09-20 17:37:12 +0300

    Completed

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,35 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre):
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+       
+        
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        
+
+    @classmethod
+    def add_song_to_count(cls, increment=1):
+        cls.count += increment
+    @classmethod
+    def add_to_genres(cls, genre):
+        cls.genres.append(genre) if genre not in cls.genres else cls.genres
+    @classmethod
+    def add_to_artists(cls,artist):
+        cls.artists.append(artist) if artist not in cls.artists else cls.genres
+    
+
+    
+
+
+
+
+pass
